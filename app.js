f28da0f7b1ba82f723b59442b12239cbf6e21294
raphael56 <raphael.hervieux@gmail.com>
2022-02-16 11:54:34 +0100

    simplification

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+
+
 const bulle = document.querySelector('.box-bille');
 const btnSlow =document.querySelector('.btn-speed-slow')
 const btnEasy =document.querySelector('.btn-speed-easy')
@@ -6,59 +9,29 @@ const TL = gsap.timeline();
 const btnPause = document.querySelector('#btn-pause')
 const btnReset = document.querySelector('#btn-reset')
 const bgcolor = document.querySelector('.container-respirateur')
+const play= document.getElementById('btn-pause')
+
+const TL1 = gsap.timeline(paused: true)
+
+TL1
+.fromTo(bulle, { y: 0 }, { duration: 6, ease: "sine.inOut", y: '42vh' })
+.fromTo(bulle, { y: 0 }, { duration: 6, ease: "sine.inOut", y: '42vh' , reversed: true})
+
+btnSlow.addEventListener('click', () => TL.play())
 
 
 
-let timer = document.querySelector('#inp-time');
-
-TL.paused(!TL.paused())
-
-
-function activation() {
-   TL.pause(0)
-   let i = 0
-    let speed = 6
-    let timer = 5
-    for (i = 0; i < timer.value *10 ; i++){
-        TL.paused(!TL.paused())
-        TL
-        .fromTo(bulle, { y: 0 }, { duration: speed, ease: "sine.inOut", y: 500 })
-        
-        .fromTo(bulle, { y: 0 }, { duration: speed, ease: "sine.inOut", y: 500 , reversed: true})
-        
-        bgcolor.style.backgroundColor = 'rgba(205, 204, 196, 1)'        
-        i++
-    }
-    console.log(TL)
-   
-}
-
-btnSlow.addEventListener('click', () => activation())
-
-
-
-// function activationBtnEasy(i) {
-//     if (btnFast.disabled == true
-//          || btnSlow.disabled == true) {
-//         btnFast.disabled = false
-//         btnSlow.disabled =false
-//     }
-//     btnEasy.disabled = true
-//     // TL.paused(!TL.paused())
-//     for (i = 0; i < timer.value *12 ; i++){
-//         if (i<1) {
-//             TL.paused(!TL.paused())
-//         }
+// function activationBtnEasy() {
+//     btnEasy.disabled= true
+//     for (i = 0; i < 5 *10 ; i++){
+    
 //         TL
-//             .fromTo(bulle, { y: 0 }, { duration: 5, ease: "sine.inOut", y: 500 })
-
-//             .fromTo(bulle, { y: 0 }, { duration: 5, ease: "sine.inOut", y: 500 , reversed: true})
-            
-//             bgcolor.style.backgroundColor = 'rgba(205, 204, 96, 1)'
+//         .fromTo(bulle, { y: 0 }, { duration: 5, ease: "sine.inOut", y: '42vh' })
+//         .fromTo(bulle, { y: 0 }, { duration: 5, ease: "sine.inOut", y: '42vh' , reversed: true})
         
+//         bgcolor.style.backgroundColor = '#6190e8'       
 //         i++
 //     }
-   
   
 // }
 
@@ -94,22 +67,17 @@ btnSlow.addEventListener('click', () => activation())
 // btnFast.addEventListener('click', () => activationBtnFast())
 
 
-// //btn de gestion pause et reset
-// btnPause.addEventListener('click', () => {
-//     TL.paused(!TL.paused())
-//     btnPause.innerHTML = TL.paused() ? "Play" : "Pause"
-// })
+//btn de gestion pause et reset
+btnPause.addEventListener('click', () => {
+    TL.play()
+})
 
-// btnReset.addEventListener('click', () => {
-//     TL.pause(0)
-//     bgcolor.style.backgroundColor = 'none'
-// })
+btnReset.addEventListener('click', () => {
+    TL.pause(0)
+    bgcolor.style.backgroundColor = 'none'
+    btnEasy.disabled == false
+    btnSlow.disabled == false
+    TL.en
+})
 
 
-//     // affichage du timer sous le input Range
-// let temps = document.getElementById('affichageDuTimer')
-
-// timer.oninput = function () {
-//     temps.textContent = timer.value
-// }
-// console.log(temps.textContent)
